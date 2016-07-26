ca77898afa043f3ff785f17139610b8a7f175360
DaFresh <yipikai7@gmail.com>
2016-07-26 20:09:11 +0200

    sanitization

--- sandbox/f_db_check.py
+++ sandbox/f_db_check.py
@@ -2,8 +2,10 @@
 
 import sqlite3
 import os
+import sys
 import time
 import datetime
+from datetime import datetime, timedelta
 
 #db_path = '~/git/meetme-selfservice/sandbox/mod.db'
 db_path = '/Users/cedric_lemarchand/git/meetme-selfservice/sandbox/mod.db'
@@ -16,7 +18,15 @@ range = ('100','200')
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
-time_now = (int(time.time()))
+time_now = datetime.now()
+time_outdated = time_now - timedelta(minutes=60)
+
+print time_now
+print time_outdated
+
+print "EXIT"
+#sys.exit()
+
 
 test_data = [('100', '123456', time_now),
                 ('101', '123456', time_now),
@@ -26,19 +36,22 @@ test_data = [('100', '123456', time_now),
 
 c.execute("DROP TABLE '%s'" % table)
 
-c.execute("CREATE TABLE IF NOT EXISTS '%s' (room_number, pin, age INTERGER)" % table)
+c.execute("CREATE TABLE IF NOT EXISTS '%s' (room_number, pin, timestamp)" % table)
 
 c.executemany("INSERT INTO '%s' VALUES (?,?,?)" % table, test_data)
 
-c.execute("CREATE TABLE IF NOT EXISTS '%s' (room_number, pin, creation_timestamp)" % table)
-
-
 c.execute("SELECT * FROM '%s' " % table)
-
-#c.execute("DELETE FROM  mod WHERE room_number < "101")
+c.execute("SELECT * FROM '%s' WHERE timestamp =< %s" % table, time_outdated)
 
 for row in c:
         print row
+
+print "debut"
+
+c.execute("SELECT * FROM '%s' " % table)
+
+#c.execute("DELETE FROM mod WHERE room_number < "101")
+
 
 conn.commit()
 conn.close()
